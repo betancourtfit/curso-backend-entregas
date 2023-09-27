b41a59b8f96f86279c893bc7d429b7867e4549ef
betancourtfit <beta.juan.c@gmail.com>
2023-09-26 23:59:35 -0300

    añadido de desafio refactor_en_login

---
+++ refactor_en_login/src/routes/users.routes.js
@@ -0,0 +1,85 @@
+import {Router } from "express"
+import { userManager } from "../dao/models/userManager.js";
+import passport from 'passport';
+
+const userRouter = Router();
+
+userRouter.get('/', async (req, res) => {
+    const { limit, page } = req.query;
+    try {
+        const users = await userManager.findAll(limit, page);
+        res.status(200).send({respuesta: 'ok', mensaje: users})
+    } catch (error){
+        res.status(400).send({respuesta: 'Error', mensaje: error})
+    }
+})
+
+userRouter.get('/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const user = await userManager.findById(id);
+        if (user)
+            res.status(200).send({respuesta: 'ok', mensaje: user})
+        else 
+            res.status(404).send({respuesta: 'Error', mensaje: 'User not found'})
+    } catch (error){
+        res.status(400).send({respuesta: 'Error', mensaje: error})
+    }
+})
+
+
+userRouter.post('/signup', passport.authenticate('signup',{failureRedirect:'/failregister'}), async (req, res) => {
+    try {
+        if(!req.user){
+            res.status(401).send({ resultado: 'Usuario invalido' });
+        }
+
+        res.status(200).send({ resultado: 'Usuario creado exitosamente.' });
+    }
+    catch (error) {
+        console.error('Hubo un error al registrar el usuario:', error);
+        res.status(500).send({ mensaje: `Error al registrar ${error}` });
+    }
+});
+
+userRouter.get('/failregister', (req, res) => {
+    console.log('Error al registrar');
+    res.status(401).send({ resultado: 'Error al registrar' });
+});
+
+userRouter.get('/github', passport.authenticate('github', { scope: ['user:email'] }), (req, res) => { 
+    res.status(200).send({ resultado: 'Usuario creado exitosamente.' });
+});
+
+userRouter.get('/githubCallback', passport.authenticate('github', {scope: ['user:email']}), (req, res) => {
+    res.status(200).send({ resultado: 'Usuario creado exitosamente.' });
+});
+
+userRouter.put('/:id', async (req, res) => {
+    const {id} = req.params
+    const {first_name, last_name, age, email, password} = req.body
+    try {
+        const user = await userManager.updateById(id, {first_name, last_name, age, email, password});
+        if (user)
+            res.status(200).send({respuesta: 'ok', mensaje: user})
+        else 
+            res.status(404).send({respuesta: 'Error', mensaje: 'User not found'})
+    } catch (error){
+        res.status(400).send({respuesta: 'Error', mensaje: error})
+    }
+})
+
+userRouter.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const user = await userManager.deleteById(id);
+        if (user)
+            res.status(200).send({respuesta: 'ok', mensaje: user})
+        else 
+            res.status(404).send({respuesta: 'Error', mensaje: 'User not found'})
+    } catch (error){
+        res.status(400).send({respuesta: 'Error', mensaje: error})
+    }
+})
+
+export default userRouter
---
+++ refactor_en_login/src/routes/views.routes.js
@@ -0,0 +1,98 @@
+import express from "express";
+import { CartManager } from "../dao/models/cartManager.js"
+
+const viewsRouter = express.Router();
+
+
+const auth = (req, res, next) => {
+    console.log('req_session',req.session.passport)
+    console.log('req_user',req.user)
+    console.log("Is Authenticated:", req.isAuthenticated());
+    console.log("Session Object:", req.session);
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        return res.redirect('/login');
+    }
+};
+
+const auth_admin = (req, res, next) => {
+    console.log("Is Authenticated:", req.isAuthenticated());
+    console.log("Session Object:", req.session);
+    if (req.isAuthenticated() && req.user.rol === 'admin') {
+        return next();
+    } else {
+        return res.redirect('/home');
+    }
+};
+
+viewsRouter.get('/home', auth, (req, res) => {
+    res.render('home', {
+        js: "home.js",
+        css: "home.css",
+        title: "Chat"
+        
+    });
+})
+
+viewsRouter.get('/realtimeproducts', auth_admin, (req, res) => {
+    res.render('realTimeProducts', {
+        css: "style.css",
+        title: "Products",
+        js: "realTimeProducts.js"
+
+    })
+})
+
+viewsRouter.get('/carts/:cid', async (req, res) => {
+    try {
+        const cid = req.params.cid;
+        const cart = await CartManager.findById(cid);
+        console.log(cart)
+
+        if (cart) {
+            res.render('carts', { products: cart.products });
+        } else {
+            res.status(404).send({ respuesta: 'Error', mensaje: 'Carrito no encontrado' });
+        }
+
+    } catch (error) {
+        res.status(400).send({ respuesta: 'Error', mensaje: error.message });
+    }
+});
+
+viewsRouter.get('/login', (req, res) => {
+    res.render('login', {
+        js: "login.js",
+        css: "home.css",
+        title: "login",
+        
+    });
+})
+
+viewsRouter.get('/logout', (req, res) => {
+    res.render('logout', {
+        js: "logout.js",
+        css: "home.css",
+        title: "logout",
+        
+    });
+})
+
+viewsRouter.get('/signup', (req, res) => {
+    res.render('signup', {
+        js: "signup.js",
+        css: "signup.css",
+        title: "signup",
+    });
+});
+
+viewsRouter.get('/chat', (req, res) => {
+    res.render('chat', {
+        js: "chat.js",
+        css: "chat.css",
+        title: "chat",
+    });
+});
+
+export default viewsRouter;
